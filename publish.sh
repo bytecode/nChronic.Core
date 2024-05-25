c23ac2aa87ba3af5224af70fb66d45c628682b7f
bytecode WS <bytecode64@gmail.com>
2024-05-25 20:29:32 +0300

    Updated Publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 dotnet nuget push src/Chronic.Core/bin/Release/*.nupkg --source https://api.nuget.org/v3/index.json --api-key [Replaced]
+dotnet nuget push src/Chronic.Core/bin/Release/*.nupkg  -s E:\LocalPackages
